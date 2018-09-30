8612a2569f57c8dbf1b45abd50479b06ffc1aaa4
wellshiniti <well.sk@live.com>
2018-09-30 12:24:01 -0300

    Aula 01 - Hello World

---
+++ Pipoca/src/br/usjt/arqdes18/pipoca/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package br.usjt.arqdes18.pipoca.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+@RequestMapping("/hello")
+public String execute(){
+System.out.println("chamou o controller hello");
+return "hello";
+}
+}
